56e5f001cada7833618300bea1cf81ee11ef6261
babar1532 <ankitkumar6262@.outlook.com>
2025-10-17 10:53:36 +0530

    done

---
+++ leetcode641.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+class MyCircularQueue{
+
+   int[] queue;
+   int value;
+   int front;
+   int rear;
+   int capacity;
+
+
+   MyCircularQueue(int size){
+
+      this.front = -1;
+      this.rear = -1;
+      this.queue = new int[];
+      this.capacity =  size;
+   }
+
+
+
+}
+
+public class leetcode641{
+  public static void main(String[] args){
+
+     System.out.println("Hello world");
+
+
+  }
+
+
+}
