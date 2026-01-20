8b4dc3bc3ef4d27d3bd06ae1814295bee189e723
babar1532 <ankitkumar6262@outlook.com>
2026-01-21 00:00:06 +0530

    prefix phase1 complete

---
+++ leetcode525.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        int prefixSum = 0;
+        int maxLength = 0;
+
+        map.put(0,-1);
+
+        for(int i = 0; i < nums.length; i++){
+
+            prefixSum += nums[i] == 0 ? -1 : 1;
+
+           if(map.containsKey(prefixSum)){
+               maxLength = Math.max(maxLength,i - map.get(prefixSum));
+           }
+           else{
+              map.put(prefixSum,i);
+           }
+        }
+
+        return maxLength;
+
+    }
+}
